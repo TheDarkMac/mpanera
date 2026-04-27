dad737537a173d27eb118afd1a07f4956ba56530
andomamy <hei.ando14@gmail.com>
2026-04-27 22:15:33 +0300

    feat: add database types

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  adapter: async () => {
-    return new PrismaPg({ connectionString: process.env.DATABASE_URL });
+  datasource: {
+    url: process.env.DATABASE_URL,
   },
 });
